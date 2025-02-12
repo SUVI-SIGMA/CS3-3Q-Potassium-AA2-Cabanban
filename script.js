f440535146d044240de2e385b76e6e62ddb58c0e
SUVI-SIGMA <smcabanban22@src.pshs.edu.ph>
2025-02-12 12:55:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function population() {
+    let initialPopulation = prompt("Enter the value for initial population: ");
+    let rateOfGrowth = prompt("Enter the value for the rate of growth: ");
+    let timeinHours = prompt("Enter the value for time (in HOURS): ");
+    let valOne = parseFloat(initialPopulation);
+    let valTwo = parseFloat(rateOfGrowth);
+    let valThree = parseFloat(timeinHours);
+    let finalPopulation = Math.round(valOne * (Math.pow(Math.E,valTwo) * (Math.pow(Math.E,valThree))));
+    
+    let location = prompt("Enter the location of the Monster: ");
+    let name = prompt("Enter the name of the Monster: ");
+
+    let conclusion = ('After ' + valThree + ' hours, the population of ' + name + ' has risen to ' + finalPopulation);
+    document.getElementById('result').innerHTML = conclusion;
+}
